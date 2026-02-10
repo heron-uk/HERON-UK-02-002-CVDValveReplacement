fd094e85f42b3c7baad5cc8f7fa43cb86e51c6e9
edward-burn <you@example.com>
2026-02-10 09:02:12 +0000

    diag code

--- diagnostics_code/cohorts/instantiate_cohorts.R
+++ diagnostics_code/cohorts/instantiate_cohorts.R
@@ -1,16 +1,13 @@
 
 codes <- importCodelist(here::here("cohorts", "codelists"), "csv")
-
+# procedures
 cdm$aortic_valve_replacement <- conceptCohort(cdm = cdm,
                                               name = "aortic_valve_replacement",
                                               conceptSet = list(aortic_valve_replacement = codes$aortic_valve_replacement),
                                               exit = "event_start_date")
 
 cdm$tavi <- cdm$aortic_valve_replacement |>
-  requireConceptIntersect(conceptSet = list(additional_tavi_route = codes$additional_tavi_route),
-                          window = c(0, 0),
-                          name = "tavi") |>
-  requireConceptIntersect(conceptSet = list(additional_tavi_context = codes$additional_tavi_context),
+    requireConceptIntersect(conceptSet = list(tavi = codes$tavi),
                           window = c(0, 0),
                           name = "tavi") |>
   renameCohort("tavi")
@@ -22,7 +19,16 @@ cdm$savr <- cdm$aortic_valve_replacement |>
                          name = "savr") |>
   renameCohort("savr")
 
+# conditions
+cdm$avd <- conceptCohort(cdm = cdm,
+                    name = "avd",
+                    conceptSet = list(aortic_stenosis = codes$aortic_stenosis,
+                                      aortic_valve_disease = codes$aortic_valve_disease),
+                    exit = "event_start_date") |>
+  exitAtObservationEnd()
+
 cdm <- bind(cdm$aortic_valve_replacement,
             cdm$tavi,
             cdm$savr,
+            cdm$avd,
             name = "study_cohorts")
