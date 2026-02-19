5cdc4b39ba0f505a2ce174f86711ae791c57e3c7
edward-burn <you@example.com>
2026-02-19 13:11:57 +0000

    extra tavi codes

--- diagnostics_code/codelist_development.R
+++ diagnostics_code/codelist_development.R
@@ -3,7 +3,10 @@
 aortic_valve_replacement_opcs4 <- c("K26.1","K26.2","K26.3","K26.4",
                                     "K26.5", # note has no mapping
                                     "K26.8","K26.9")
-tavi_opcs4 <- c("Y49.4", "Y79.1", "Y79.2", "Y79.3", "Y79.4", "Y79.5", "Y79.8", "Y79.9")
+tavi_additional_opcs4 <- c("Y49.4", "Y79.1", "Y79.2", "Y79.3", "Y79.4", "Y79.5",
+                           "Y79.8", "Y79.9")
+tavi <- list(tavi = c(36676805L, 37157401L)) |>
+  omopgenerics::newCodelist()
 
 opcs4_to_standard <- function(opcs4_codes){
 cdm_vocab_2025_08$concept |>
@@ -19,14 +22,17 @@ cdm_vocab_2025_08$concept |>
 aortic_valve_replacement <- list(
   aortic_valve_replacement = opcs4_to_standard(aortic_valve_replacement_opcs4)) |>
   omopgenerics::newCodelist()
-tavi <- list(
-  tavi = opcs4_to_standard(tavi_opcs4)) |>
+tavi_additional <- list(
+  tavi_additional = opcs4_to_standard(tavi_additional_opcs4)) |>
   omopgenerics::newCodelist()
 
 omopgenerics::exportCodelist(aortic_valve_replacement,
                              here::here("cohorts", "codelists"), "csv")
+omopgenerics::exportCodelist(tavi_additional,
+                             here::here("cohorts", "codelists"), "csv")
 omopgenerics::exportCodelist(tavi,
                              here::here("cohorts", "codelists"), "csv")
+
 
 # systematic search for aortic valve disease ----
 library(writexl)
@@ -39,11 +45,7 @@ avd <- getCandidateCodes(cdm_vocab_2025_08,
                          includeDescendants = TRUE)
 write_xlsx(list("codes_for_review" = avd |>
                   mutate(aortic_valve_disease = NA_character_,
-                         aortic_stenosis = NA_character_,
-                         aortic_regurgitation  = NA_character_,
-                         mixed_aortic_alve_disease  = NA_character_),
-                "read_code_mappings" = avd_read_mappings,
-                "icd_code_mappings" = avd_icd_mappings),
+                         aortic_stenosis = NA_character_)),
            "aortic_valve_disease_codes_for_review.xlsx")
 
 # codes reviewed
--- diagnostics_code/cohorts/instantiate_cohorts.R
+++ diagnostics_code/cohorts/instantiate_cohorts.R
@@ -6,11 +6,22 @@ cdm$aortic_valve_replacement <- conceptCohort(cdm = cdm,
                                               conceptSet = list(aortic_valve_replacement = codes$aortic_valve_replacement),
                                               exit = "event_start_date")
 
-cdm$tavi <- cdm$aortic_valve_replacement |>
-    requireConceptIntersect(conceptSet = list(tavi = codes$tavi),
+# tavi is either based on additional codes for a avr, or from tavi specific codes
+cdm$tavi_from_additional <- cdm$aortic_valve_replacement |>
+    requireConceptIntersect(conceptSet = list(tavi_additional = codes$tavi_additional),
                           window = c(0, 0),
-                          name = "tavi") |>
+                          name = "tavi_from_additional")
+cdm$tavi_direct <- conceptCohort(cdm = cdm,
+                                 name = "tavi_direct",
+                                 conceptSet = list(tavi = codes$tavi),
+                                 exit = "event_start_date")
+
+cdm <- bind(cdm$tavi_from_additional,
+                 cdm$tavi_direct,
+                 name = "tavi") |>
+  unionCohorts() |>
   renameCohort("tavi")
+
 
 cdm$savr <- cdm$aortic_valve_replacement |>
   requireCohortIntersect(targetCohortTable = "tavi",
