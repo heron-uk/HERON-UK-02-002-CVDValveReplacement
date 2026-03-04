11349eaa553eb59196e63978503c1785a1ee3763
martaalcalde <marta.alcalde@estudiantat.upc.edu>
2026-03-04 22:09:53 +0000

    fix obj34 typo

--- study_code_avr/analyses/3-ObjectiveThreeAndFour.R
+++ study_code_avr/analyses/3-ObjectiveThreeAndFour.R
@@ -3,17 +3,17 @@ omopgenerics::logMessage(message = "STARTING OBJECTIVES 3 AND 4")
 cdm[["proc"]] <- cdm[["proc"]] |>
   mutate("calendar_year" = get_year(cohort_start_date))
 
-summariseCharacteristics(cdm[["proc"]], 
-                         strata = c("sex", "age_group", "calendar_year", c("calendar_year", "sex"), c("calendar_year", "age_group")),
-                         cohortIntersectFlag = 
-                           list(
-                             "Comorbidities one year prior to index date" = list(
-                               targetCohortTable = "comorbidities",
-                               window = c(-365, 0)
-                             ),
-                             "Treatments 31 days prior to index date" = list(
-                               targetCohortTable = "treatments",
-                               window = c(-31, 0)
-                             )))
+results[["objective_three_and_four"]] <- summariseCharacteristics(cdm[["proc"]], 
+                                                                  strata = list("sex", "age_group", "calendar_year", c("calendar_year", "sex"), c("calendar_year", "age_group")),
+                                                                  cohortIntersectFlag = 
+                                                                    list(
+                                                                      "Comorbidities one year prior to index date" = list(
+                                                                        targetCohortTable = "comorbidities",
+                                                                        window = c(-365, 0)
+                                                                      ),
+                                                                      "Treatments 31 days prior to index date" = list(
+                                                                        targetCohortTable = "treatments",
+                                                                        window = c(-31, 0)
+                                                                      )))
 
 omopgenerics::logMessage(message = "OBJECTIVES 3 AND 4 FINISHED")
