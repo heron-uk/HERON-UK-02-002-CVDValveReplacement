b9b570fdb42aa7adea3a7ee919e5a152310146e9
cecicampanile <campanile.cecilia@gmail.com>
2026-03-04 13:48:26 +0000

    model

--- study_code/analyses/multi_state_model.R
+++ study_code/analyses/multi_state_model.R
@@ -1,5 +1,6 @@
 source(here::here("analyses", "functions.R"))
 
+omopgenerics::logMessage(message = "Fit multi state model")
 
 cohort_names <- cdm$multi_state_as |> omopgenerics::getCohortName() 
 cdm$multi_state_as <- PatientProfiles::addCohortName(cdm$multi_state_as)
@@ -8,20 +9,42 @@ cdm$multi_state_as <- PatientProfiles::addCohortName(cdm$multi_state_as)
 model_specs <- list(
   age_sex = c("age", "sex"),
   ses_age_sex = c("age", "sex", "ses")
-  # eth_age_sex = c("ethnicity_group", "age_group", "sex")
 )
 
 res <- list()
+
+
 for (cohort_name in cohort_names) {
-  data <- cdm$multi_state_as |> dplyr::filter(.data$cohort_name == cohort_name) |> dplyr::collect()
-  f_lin  <- survival::Surv(t_start, t_stop, status) ~ age+ strata(trans)
-  f_rcs3 <- survival::Surv(t_start, t_stop, status) ~ rms::rcs(age, 3) + strata(trans)
-  f_rcs4 <- survival::Surv(t_start, t_stop, status) ~ rms::rcs(age, 4) + strata(trans)
+  data <- cdm$multi_state_as |> dplyr::filter(.data$cohort_name == cohort_name) |> dplyr::collect() 
+  
+  dd <- rms::datadist(data)
+  options(datadist = "dd")
+  flin  <- rms::Surv(t_start, t_stop, status) ~ age
+  f_rcs3 <- rms::Surv(t_start, t_stop, status) ~ rms::rcs(age, 3)
+  f_rcs4 <-rms::Surv(t_start, t_stop, status) ~ rms::rcs(age, 4) 
   fit <- list()
-  fit[["linear"]] <- survival::coxph(f_lin,  data = data, cluster = data$subject_id)
-  fit[["rcs3"]] <- survival::coxph(f_rcs3, data = data, cluster = data$subject_id)
-  fit[["rcs4"]] <- survival::coxph(f_rcs4, data = data, cluster = data$subject_id)
-  crit <- c(linear = stats::AIC(fit_lin), rcs3 = stats::AIC(fit_rcs3), rcs4 = stats::AIC(fit_rcs4))
+  fit[["linear"]] <- rms::cph(
+    flin,
+    data = data,
+    x = TRUE,
+    y = TRUE,
+    surv = TRUE
+  )
+  fit[["rcs3"]] <- rms::cph(
+    f_rcs3,
+    data = data,
+    x = TRUE,
+    y = TRUE,
+    surv = TRUE
+  )
+  fit[["rcs4"]] <-rms::cph(
+    f_rcs4,
+    data = data,
+    x = TRUE,
+    y = TRUE,
+    surv = TRUE
+  )
+  crit <- c(linear = stats::AIC(fit[["linear"]]), rcs3 = stats::AIC(fit[["rcs3"]]), rcs4 = stats::AIC(fit[["rcs4"]]))
   chosen_age <- names(which.min(crit)) 
   chosen_age_term <- switch(chosen_age,
                             linear = "age",
@@ -30,7 +53,7 @@ for (cohort_name in cohort_names) {
   
   
     
-  res[[paste(cohort_name,"age",chosen_age, sep = "_")]] <- hr_summary(summary(fit[[chosen_age]]), transition = cohort_name, model_name = paste0("age_",chosen_age))
+  res[[paste(cohort_name,"age",chosen_age, sep = "_")]] <- hr_summary_age_model(fit[[chosen_age]], transition = cohort_name, model_name = paste0("age_",chosen_age))
   
   
   for (nm in names(model_specs)) {
@@ -41,17 +64,17 @@ for (cohort_name in cohort_names) {
     
     
     form_base <- stats::as.formula(
-      paste0("survival::Surv(t_start, t_stop, status) ~ ",
-             rhs,
-             " + strata(trans)")
+      paste0("Surv(t_start, t_stop, status) ~ ", rhs)
     )
     
-   cox_model <- survival::coxph(
-      form_base,
-      data = data,
-      cluster = subject_id
-    )
-   res[[paste(cohort_name,nm, sep = "_")]] <- hr_summary(summary(cox_model), transition = cohort_name, model_name = nm)
+  model <- rms::cph(
+     form_base,
+     data = data,
+     x = TRUE,
+     y = TRUE,
+     surv = TRUE
+   )
+   res[[paste(cohort_name,nm, sep = "_")]] <- hr_summary(model, transition = cohort_name, model_name = nm)
     }
 
 }
