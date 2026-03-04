b1d50a89b6722fd2a45ef94e44a98ff27a3bb273
edward-burn <you@example.com>
2026-03-04 17:39:27 +0000

    - rm exclusions for incidence - move denominator into cohort file

--- study_code/cohorts/instantiate_cohorts.R
+++ study_code/cohorts/instantiate_cohorts.R
@@ -12,25 +12,30 @@ codelist <- omopgenerics::importCodelist(here::here("codelist"), "csv")
 omopgenerics::logMessage(message = "Instastiating cohorts")
 
 cdm$study_cohorts_inc <- CohortConstructor::conceptCohort(cdm = cdm, 
-                                           conceptSet = c(codelist["aortic_stenosis"], codelist["aortic_valve_replacement"]), 
+                                           conceptSet = c(codelist["aortic_stenosis"], 
+                                                          codelist["aortic_valve_replacement"]), 
                                            name = "study_cohorts_inc", 
                                            exit = "event_start_date"
-                                           ) |>
-  CohortConstructor::requireIsFirstEntry() |>
-  CohortConstructor::exitAtObservationEnd(cohortId = c("aortic_stenosis")) |>
+                                           )
+  
+cdm$study_cohorts <- cdm$study_cohorts_inc |>
+  CohortConstructor::requireIsFirstEntry(name = "study_cohorts") |>
+  CohortConstructor::exitAtObservationEnd(cohortId = c("aortic_stenosis"),
+                                          name = "study_cohorts") |>
   CohortConstructor::requireConceptIntersect(conceptSet = codelist["aortic_stenosis"], 
                                              cohortId = "aortic_valve_replacement", 
                                              intersections = c(1, Inf), 
-                                             window = c(-Inf, 0)) 
-  
-
-cdm$study_cohorts <- cdm$study_cohorts_inc |>
+                                             window = c(-Inf, 0), 
+                                             name = "study_cohorts") |>
   CohortConstructor::requireConceptIntersect(conceptSet = codelist["aortic_valve_replacement"], 
                                              cohortId = "aortic_stenosis", 
                                              intersections = 0L, 
-                                             window = c(-Inf, -1)) |>
-  CohortConstructor::requireAge(ageRange = list(c(20, Inf)), name = "study_cohorts") |>
-  CohortConstructor::requireInDateRange(dateRange = study_period) |>
+                                             window = c(-Inf, -1), 
+                                             name = "study_cohorts") |>
+  CohortConstructor::requireAge(ageRange = list(c(20, Inf)), 
+                                name = "study_cohorts") |>
+  CohortConstructor::requireInDateRange(dateRange = study_period, 
+                                        name = "study_cohorts") |>
   addEthnicity() |>
   addSES() |>
   PatientProfiles::addDemographics(sex = sex, 
@@ -41,6 +46,21 @@ cdm$study_cohorts <- cdm$study_cohorts_inc |>
                                    name = "study_cohorts") 
 
 
+omopgenerics::logMessage(message = "Get denominator cohort")
 
+cdm <- IncidencePrevalence::generateDenominatorCohortSet(
+  cdm = cdm, 
+  name = "denominator",
+  cohortDateRange = study_period,
+  ageGroup = c(list(
+    c(20, 150)), 
+    age_groups),
+  sex = c("Both", "Male", "Female"),
+  daysPriorObservation = 365) 
 
+omopgenerics::logMessage(message = "Add socioeconomic status")
+
+cdm$denominator <- cdm$denominator |>
+  addSES() |>
+  dplyr::compute(name = "denominator") 
 
