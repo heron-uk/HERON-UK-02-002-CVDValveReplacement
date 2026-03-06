6ce274169593346b6f7d37d05169c8254763d953
foluwa <moronfoluwaakintola@gmail.com>
2026-03-06 09:34:48 +0000

    Fix cohort changes

--- congenital_study_code/cohorts/instantiate_cohorts.R
+++ congenital_study_code/cohorts/instantiate_cohorts.R
@@ -1,45 +1,28 @@
 codes <- CodelistGenerator::importCodelist(here::here("cohorts", "codelists"), "csv")
 
-# Create study cohorts directly - congenital AS and AVD (ages 0-17)
-cdm$congenital_aortic_stenosis <- CohortConstructor::conceptCohort(
+# Create study cohorts - congenital AS and AVD (ages 0-17)
+# Create both in a single cohort table
+cdm$study_cohorts <- CohortConstructor::conceptCohort(
   cdm = cdm,
-  name = "congenital_aortic_stenosis",
+  name = "study_cohorts",
   conceptSet = list(
-    congenital_aortic_stenosis = codes$aortic_stenosis
-  ),
-  exit = "event_start_date"
-) |>
-  CohortConstructor::requireIsFirstEntry() |>
-  CohortConstructor::exitAtObservationEnd() |>
-  CohortConstructor::requireAge(ageRange = c(0, 17), name = "congenital_aortic_stenosis")
-
-cdm$congenital_aortic_valve_disease <- CohortConstructor::conceptCohort(
-  cdm = cdm,
-  name = "congenital_aortic_valve_disease",
-  conceptSet = list(
+    congenital_aortic_stenosis = codes$aortic_stenosis,
     congenital_aortic_valve_disease = codes$aortic_valve_disease
   ),
   exit = "event_start_date"
 ) |>
   CohortConstructor::requireIsFirstEntry() |>
   CohortConstructor::exitAtObservationEnd() |>
-  CohortConstructor::requireAge(ageRange = c(0, 17), name = "congenital_aortic_valve_disease")
-
-# Bind the two cohorts into study_cohorts
-cdm <- omopgenerics::bind(
-  cdm$congenital_aortic_stenosis,
-  cdm$congenital_aortic_valve_disease,
-  name = "study_cohorts"
-)
+  CohortConstructor::requireAge(ageRange = c(0, 17))
 
 # Intervention cohorts for survival analysis 
-# AVR intervention (all ages, no restriction)
+# AVR intervention (all ages, first ever, exit same day)
 cdm$intervention_cohorts <- CohortConstructor::conceptCohort(
   cdm = cdm,
   name = "intervention_cohorts",
   conceptSet = list(
     avr = codes$aortic_valve_replacement
   ),
-  exit = "event_start_date"
+  exit = "event_start_date"  
 ) |>
-  CohortConstructor::exitAtObservationEnd()
+  CohortConstructor::requireIsFirstEntry()  # First ever in history
