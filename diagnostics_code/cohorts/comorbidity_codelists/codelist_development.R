951b62adbdb972415dda14413f4767910c661b3b
martaalcalde <marta.alcalde@estudiantat.upc.edu>
2026-03-08 09:17:59 +0000

    comorbidities and treatments codelists

--- diagnostics_code/cohorts/comorbidity_codelists/codelist_development.R
+++ diagnostics_code/cohorts/comorbidity_codelists/codelist_development.R
@@ -23,6 +23,28 @@ x <- getCandidateCodes(cdm_vocab_2025_08,
 
 write_csv(x, file = here("cohorts", "comorbidity_codelists", "left_ventricular_hypertrophy.csv"))
 
+x <- getCandidateCodes(cdm_vocab_2025_08,
+                       keywords = c("pacemaker", "defibrillator"),
+                       domains = c("Procedure", "Observation", "Condition"))
 
+write_csv(x, file = here("cohorts", "comorbidity_codelists", "pre_existing_pacemaker_or_defibrillator.csv"))
+
+x <- getCandidateCodes(cdm_vocab_2025_08,
+                       keywords = c("peripheral arterial disease"),
+                       domains = c("Condition"))
+
+write_csv(x, file = here("cohorts", "comorbidity_codelists", "peripheral_arterial_disease.csv"))
+
+x <- getCandidateCodes(cdm_vocab_2025_08,
+                       keywords = c("left bundle branch block"),
+                       domains = c("Condition"))
+
+write_csv(x, file = here("cohorts", "comorbidity_codelists", "left_bundle_branch_block.csv"))
+
+x <- getCandidateCodes(cdm_vocab_2025_08,
+                       keywords = c("right bundle branch block"),
+                       domains = c("Condition"))
+
+write_csv(x, file = here("cohorts", "comorbidity_codelists", "right_bundle_branch_block.csv"))
 
 
