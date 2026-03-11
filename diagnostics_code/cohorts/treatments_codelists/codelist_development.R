209a01cd79cc490313dfe59bed203389e63e7bd3
martaalcalde <marta.alcalde@estudiantat.upc.edu>
2026-03-11 16:31:07 +0000

    add treatments codelists

--- diagnostics_code/cohorts/treatments_codelists/codelist_development.R
+++ diagnostics_code/cohorts/treatments_codelists/codelist_development.R
@@ -1,3 +1,36 @@
+# ACEi and ARBs -------
+acei_arbs <- getDrugIngredientCodes(
+  cdm = cdm_vocab_2025_08,
+  name = c(# acei
+    "captopril", "enalapril", "fosinopril",
+    "imidapril", "lisinopril", "perindopril", "quinapril",
+    "ramipril", "trandolapril",
+    # arbs
+    "azilsartan", "candesartan", "eprosartan", "irbesartan",
+    "losartan", "olmesartan", "telmisartan", "valsartan"),
+  nameStyle = "{concept_name}",
+  type = "codelist") |>
+  unionCodelists()
+names(acei_arbs) <- "acei_arbs"
+exportCodelist(acei_arbs, path = here::here("Cohorts", "drugs"),
+               type = "csv")
+
+# beta blockers -------
+beta_blockers <- getDrugIngredientCodes(
+  cdm = cdm_vocab_2025_08,
+  name = c("acebutolol", "alprenolol", "atenolol",
+           "bisoprolol", "carvedilol",
+           "metoprolol", "nadolol",
+           "oxprenolol", "pindolol",
+           "propranolol", "timolol"),
+  nameStyle = "{concept_name}",
+  type = "codelist") |>
+  unionCodelists()
+names(beta_blockers) <- "beta_blockers"
+exportCodelist(beta_blockers, path = here::here("Cohorts", "drugs"),
+               type = "csv")
+
+
 x <- getDrugIngredientCodes(cdm_vocab_2025_08,
                             name = c("spironolactone", "eplerenone"),
                             nameStyle = "{concept_name}",
