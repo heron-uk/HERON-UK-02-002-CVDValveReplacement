0d1883678fdaeee3e955e8b7bfbcc1727d1f74ba
martaalcalde <marta.alcalde@estudiantat.upc.edu>
2026-03-15 12:09:43 +0000

    upload codelists avr and indications

--- valve_replacement_study_code/rw_clinical_practice_study/Report/get_codelists_details.R
+++ valve_replacement_study_code/rw_clinical_practice_study/Report/get_codelists_details.R
@@ -2,4 +2,5 @@ x <- CodelistGenerator::importCodelist(path = here::here("cohorts", "study_codel
 x <- CodelistGenerator::asCodelistWithDetails(x, cdm_vocab_2025_08)
 
 purrr::imap_dfr(x, ~ dplyr::mutate(.x, codelist_name = .y)) |>
-  readr::write_csv("codelists.csv")
+  dplyr::filter(codelist_name %in% c("aortic_stenosis_avr", "aortic_endocarditis_avr", "aortic_insufficiency_avr")) |>
+  readr::write_csv(here::here("Report", "codelists.csv"))
